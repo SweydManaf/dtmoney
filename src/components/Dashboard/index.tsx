94e7ce27641a54241b6be0bfca823795823d3e6d
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-07-12 21:07:58 +0200

    init

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+import { TransactionsTable } from "../TransactionsTable";
+
+
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Summary />
+            <TransactionsTable />
+        </Container>
+    )
+}
