35f64ad21c269199aad3c6bdc0958eaa5deb399e
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-07-16 22:01:21 +0200

    calculando o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,27 @@ import { useContext } from "react";
 export function Summary() {
   const { transactions } = useContext(TransactionContext);
 
+  // const totalDeposit = transactions.reduce((acc, transaction) => {
+  //   if (transaction.type == "deposit") {
+  //     return acc + transaction.amount;
+  //   }
+  //   return acc;
+  // }, 0);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    { deposits: 0, withdraws: 0, total: 0 }
+  );
+
   return (
     <Container>
       <div>
@@ -15,21 +36,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={icomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -{" "}
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="hightlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
